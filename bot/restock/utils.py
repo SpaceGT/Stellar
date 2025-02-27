4537361e14a0b760c076d89334e4a55e61050299
Space <unclearspace@gmail.com>
2025-02-27 18:21:44 +0000

    Fix market alerts

--- bot/restock/utils.py
+++ bot/restock/utils.py
@@ -94,7 +94,7 @@ async def write_market_alert(
     if (
         last_message
         and datetime.now(timezone.utc)
-        - last_message[1].created_at.astimezone(timezone.utc)
+        - last_message[0].created_at.astimezone(timezone.utc)
         < TIMINGS.market_followup
     ):
         return
